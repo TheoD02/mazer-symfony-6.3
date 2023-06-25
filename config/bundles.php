433a52d15b8646cc1970cbe1ad25f9c905871694
TheoD02 <theo.d02290@gmail.com>
2023-06-25 23:07:03 +0200

    Remove deprecated package

--- config/bundles.php
+++ config/bundles.php
@@ -34,9 +34,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => [
         'dev' => true,
     ],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => [
-        'all' => true,
-    ],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => [
         'all' => true,
     ],
